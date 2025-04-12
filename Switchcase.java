a3813eb62e4f6175c5729b365173cf7e8bf702b5
Himanshu kumar <himanshukr1505892@gmail.com>
2025-04-12 07:42:05 +0530

    first commit

---
+++ Switchcase.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+
+public class SwitchCase {
+    public static void main (String args[]){
+        int number=4;
+        switch(number){
+            case 1 :
+                System.out.println("Samosa");
+                break;
+            case 2 :
+                System.out.println("Burger");
+                break;
+            case 3 :
+                System.out.println("Pizza");
+                break;
+            case 4 :    
+                System.out.println("we woke up");
+                break;
+        }
+    }
+}
